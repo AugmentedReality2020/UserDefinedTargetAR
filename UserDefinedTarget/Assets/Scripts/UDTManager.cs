28ec6e7e225d5f1daebf6c1e9b53c1a80966290d
Naveenasri02 <66512636+Naveenasri02@users.noreply.github.com>
2020-11-04 20:24:42 +0530

    first commit

---
+++ UserDefinedTarget/Assets/Scripts/UDTManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class UDTManager : MonoBehaviour,IUserDefinedTargetEventHandler
+{
+    UserDefinedTargetBuildingBehaviour udt_targetBuildingBehavior;
+    ObjectTracker objectTracker;
+    DataSet dataSet;
+    ImageTargetBuilder.FrameQuality udt_FrameQuality;  
+    public ImageTargetBehaviour targetBehaviour;
+    int targetCounter;
+    void Start()
+    {
+        udt_targetBuildingBehavior = GetComponent<UserDefinedTargetBuildingBehaviour>();
+        if(udt_targetBuildingBehavior)
+        {
+            udt_targetBuildingBehavior.RegisterEventHandler(this);
+        }
+    }
+    //This method update the frameQuality
+    public void OnFrameQualityChanged(ImageTargetBuilder.FrameQuality frameQuality)
+    {
+        udt_FrameQuality = frameQuality;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnInitialized()
+    {
+        objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        if(objectTracker!=null)
+        {
+            dataSet = objectTracker.CreateDataSet();
+            objectTracker.ActivateDataSet(dataSet);
+        }
+    }
+
+    public void OnNewTrackableSource(TrackableSource trackableSource)
+    {
+        targetCounter++;
+        objectTracker.DeactivateDataSet(dataSet);
+        dataSet.CreateTrackable(trackableSource, targetBehaviour.gameObject);
+        objectTracker.ActivateDataSet(dataSet);
+        udt_targetBuildingBehavior.StartScanning();
+
+        //  throw new System.NotImplementedException();
+    }
+    public void BuildTarget()
+    {
+        if(udt_FrameQuality==ImageTargetBuilder.FrameQuality.FRAME_QUALITY_HIGH)
+        {
+            udt_targetBuildingBehavior.BuildNewTarget(targetCounter.ToString(), targetBehaviour.GetSize().x);
+        }
+    }
+}
